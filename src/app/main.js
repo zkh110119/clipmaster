c7cfab3ea98a7551f71a9ab0884147ba015ccda3
钟凯华 <493014828@qq.com>
2020-07-06 16:49:11 +0800

    包安装同步

--- src/app/main.js
+++ src/app/main.js
@@ -3,8 +3,19 @@ const {app, Menu, Tray} = require('electron');
 
 let tray = null;
 app.on('ready', () => {
+    if (app.dock) {
+        app.dock.hide();
+    }
     tray = new Tray(path.join(__dirname, '/Icon.png'));
     if (process.platform === 'win32') {
         tray.on('click', tray.popUpContextMenu);
     }
+    const menu = Menu.buildFromTemplate([{
+        label: 'Quit',
+        click() {
+            app.quit();
+        }
+    }]);
+    tray.setToolTip('Clipmaster');
+    tray.setContextMenu(menu);
 });
